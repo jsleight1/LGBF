8f9a6bd9a0b720331767337c094ec4bdfc68496a
jsleight1 <50985034+jsleight1@users.noreply.github.com>
2025-03-20 18:55:39 +0000

    Fix shinytest2 ci tests (#8)

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -7,7 +7,6 @@ with_mocked_bindings(
 
 test_that("LGBF app initial values works", {
     skip_on_cran()
-    skip_on_ci()
 
     app <- AppDriver$new(shiny_app, name = "initial", width = 800, height = 700,
         seed = 4323)
@@ -20,10 +19,9 @@ test_that("LGBF app initial values works", {
 
 test_that("LGBF app indicator area visualisation works", {
     skip_on_cran()
-    skip_on_ci()
 
     app <- AppDriver$new(shiny_app, name = "visualisation", width = 800, height = 700,
-        seed = 4323, load_timeout = 40 * 1000)
+        seed = 4323, load_timeout = 80 * 1000)
 
     app$set_inputs(sidebar = "visual_Adult_Social_Care_Services")
     app$wait_for_value(output = "Adult_Social_Care_Services-SW01-indicator")
@@ -36,7 +34,6 @@ test_that("LGBF app indicator area visualisation works", {
 
 test_that("LGBF app indicator area datatable works", {
     skip_on_cran()
-    skip_on_ci()
 
     app <- AppDriver$new(shiny_app, name = "datatable", width = 800, height = 700,
         seed = 4323)
